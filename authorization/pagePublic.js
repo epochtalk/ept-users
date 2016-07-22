7bb1ddd46aa8c6bb9547636ac7bf13f629205965
Anthony Kinsey <anthony@slickage.com>
2016-07-22 10:27:08 -1000

    Authorization code cleanup

--- authorization/pagePublic.js
+++ authorization/pagePublic.js
@@ -1,15 +1,11 @@
 var Boom = require('boom');
 
-module.exports = function userFind(server, auth) {
-
-  // check base Permission
-  var allowed = server.authorization.build({
+module.exports = function(server, auth) {
+  return server.authorization.build({
     error: Boom.forbidden(),
     type: 'hasPermission',
     server: server,
     auth: auth,
     permission: 'users.pagePublic.allow'
   });
-
-  return allowed;
 };
