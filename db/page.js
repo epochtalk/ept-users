790562a8d62d56b0da0f238650020451448bbdbf
Anthony Kinsey <anthony@slickage.com>
2016-07-29 12:28:47 -1000

    Updated queries to support searching users by ip or ip wildcard

--- db/page.js
+++ db/page.js
@@ -7,10 +7,10 @@ var helper = dbc.helper;
 module.exports = function(opts) {
   var q;
   if (opts && opts.filter && opts.filter === 'banned') {
-    q = 'SELECT u.id, u.username, u.email, u.deleted, u.created_at, u.updated_at, u.imported_at, b.expiration as ban_expiration FROM users u RIGHT JOIN (SELECT ub.expiration, ub.user_id FROM users.bans ub WHERE ub.expiration > now()) b ON (u.id = b.user_id)';
+    q = 'SELECT u.id, u.username, u.email, u.deleted, u.created_at, u.updated_at, u.imported_at, b.expiration as ban_expiration, ARRAY(SELECT ip.user_ip FROM users.ips ip WHERE u.id = ip.user_id) as user_ips FROM users u RIGHT JOIN (SELECT ub.expiration, ub.user_id FROM users.bans ub WHERE ub.expiration > now()) b ON (u.id = b.user_id)';
   }
   else {
-    q = 'SELECT u.id, u.username, u.email, u.deleted, u.created_at, u.updated_at, u.imported_at, (SELECT ub.expiration FROM users.bans ub WHERE ub.user_id = u.id AND ub.expiration > now()) as ban_expiration FROM users u';
+    q = 'SELECT u.id, u.username, u.email, u.deleted, u.created_at, u.updated_at, u.imported_at, (SELECT ub.expiration FROM users.bans ub WHERE ub.user_id = u.id AND ub.expiration > now()) as ban_expiration, ARRAY(SELECT ip.user_ip FROM users.ips ip WHERE u.id = ip.user_id) as user_ips FROM users u';
   }
 
   opts = opts || {};
@@ -21,13 +21,24 @@ module.exports = function(opts) {
   var order = opts.sortDesc ? 'DESC' : 'ASC';
   var params;
   if (opts && opts.searchStr) {
-    q = [q, 'WHERE u.username LIKE $1 ORDER BY', sortField, order, 'LIMIT $2 OFFSET $3'].join(' ');
-    params = [opts.searchStr + '%', limit, offset];
+    if (opts.ip && opts.filter === 'banned') {
+      q = [q, 'INNER JOIN users.ips i ON (u.id = i.user_id AND i.user_ip LIKE $1) GROUP BY u.id, b.expiration ORDER BY', sortField, order, 'LIMIT $2 OFFSET $3'].join(' ');
+      params = [opts.searchStr, limit, offset];
+    }
+    else if (opts.ip) {
+      q = [q, 'INNER JOIN users.ips i ON (u.id = i.user_id AND i.user_ip LIKE $1) GROUP BY u.id ORDER BY', sortField, order, 'LIMIT $2 OFFSET $3'].join(' ');
+      params = [opts.searchStr, limit, offset];
+    }
+    else {
+      q = [q, 'WHERE u.username LIKE $1 ORDER BY', sortField, order, 'LIMIT $2 OFFSET $3'].join(' ');
+      params = [opts.searchStr + '%', limit, offset];
+    }
   }
   else {
     q = [q, 'ORDER BY', sortField, order, 'LIMIT $1 OFFSET $2'].join(' ');
     params = [limit, offset];
   }
+
   return db.sqlQuery(q, params)
   .then(helper.slugify);
 };
